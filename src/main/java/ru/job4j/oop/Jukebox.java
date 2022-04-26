5e646d2db0788b0773aadf96c68fd420a7e475f9
leyla1991 <atagleyla@mail.ru>
2022-04-26 09:29:51 +0300

    1.3. Вызов метода с аргументами. [#166086 #293564]

--- src/main/java/ru/job4j/oop/Jukebox.java
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -4,18 +4,24 @@ public class Jukebox {
 
     public void music(int position) {
         if (position == 1) {
-            System.out.println("Пусть бегут неуклюже\n" + "Пешеходы по лужам,\n"
-                    + "А вода - по асфальту рекой.\n" + "И неясно прохожим\n" + "В этот день непогожий,\n" + "Почему я веселый такой.\n"
-                    + "\n" + "Припев:\n" + "Я играю на гармошке\n" + "У прохожих на виду...\n" + "К сожаленью, день рожденья\n"
-                    + "Только раз в году.\n" + "К сожаленью, день рожденья\n" + "Только раз в году.\n" + "\n" + "Прилетит вдруг волшебник\n"
-                    + "В голубом вертолете\n" + "И бесплатно покажет кино,\n" + "С днем рожденья поздравит\n" + "И, наверно, оставит\n" + "Мне в подарок пятьсот \"эскимо\".\n");
+            System.out.println("Пусть бегут неуклюже" + System.lineSeparator() +"Пешеходы по лужам," + System.lineSeparator()
+                    + "А вода - по асфальту рекой." + System.lineSeparator() + "И неясно прохожим" + System.lineSeparator()
+                    + "В этот день непогожий," + System.lineSeparator() + "Почему я веселый такой." + System.lineSeparator()
+                    + "Припев:" + System.lineSeparator() +"Я играю на гармошке" + System.lineSeparator() + "У прохожих на виду..." + System.lineSeparator()
+                    + "К сожаленью, день рожденья" + System.lineSeparator() + "Только раз в году." + System.lineSeparator()
+                    + "К сожаленью, день рожденья" + System.lineSeparator() + "Только раз в году." + System.lineSeparator()
+                    + "Прилетит вдруг волшебник" + System.lineSeparator() + "В голубом вертолете" + System.lineSeparator()
+                    + "И бесплатно покажет кино," + System.lineSeparator() +"С днем рожденья поздравит" + System.lineSeparator()
+                    + "И, наверно, оставит" + System.lineSeparator() + "Мне в подарок пятьсот \"эскимо\"." + System.lineSeparator());
         } else if (position == 2) {
-            System.out.println("Спят усталые игрушки, книжки спят,\n"
-                    + "Одеяла и подушки ждут ребят.\n" + "Даже сказка спать ложится,\n" + "Чтобы ночью нам присниться.\n"
-                    + "Ты ей пожелай: «Баю-бай!»\n" + "\n" + "В сказке можно покататься на Луне,\n" + "И по радуге промчаться на коне,\n"
-                    + "Со слонёнком подружиться,\n" + "И поймать перо Жар — птицы,\n" + "Ты ей пожелай — Баю — бай.\n" + "\n"
-                    + "Баю-бай, должны все люди ночью спать.\n" + "Баю-баю, завтра будет день опять.\n" + "За день мы устали очень,\n"
-                    + "Скажем всем: «Спокойной ночи!»\n" + "Глазки закрывай! Баю-бай!\n");
+            System.out.println("Спят усталые игрушки, книжки спят," + System.lineSeparator()
+                    + "Одеяла и подушки ждут ребят." + System.lineSeparator() + "Даже сказка спать ложится," + System.lineSeparator()
+                    +"Чтобы ночью нам присниться." + System.lineSeparator() + "Ты ей пожелай: «Баю-бай!»" + System.lineSeparator()
+                    + "В сказке можно покататься на Луне," + System.lineSeparator() + "И по радуге промчаться на коне," + System.lineSeparator()
+                    + "Со слонёнком подружиться," + System.lineSeparator() + "И поймать перо Жар — птицы," + System.lineSeparator()
+                    + "Ты ей пожелай — Баю — бай." + System.lineSeparator() + "Баю-бай, должны все люди ночью спать." + System.lineSeparator()
+                    + "Баю-баю, завтра будет день опять." + System.lineSeparator() + "За день мы устали очень," + System.lineSeparator()
+                    + "Скажем всем: «Спокойной ночи!»" + System.lineSeparator() + "Глазки закрывай! Баю-бай!" + System.lineSeparator());
         } else {
             System.out.println("Песня не найдена\n");
         }
