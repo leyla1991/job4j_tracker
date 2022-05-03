39d0ee7912716399df21545cac7d8a4466d76ecb
leyla1991 <atagleyla@mail.ru>
2022-05-03 10:37:26 +0300

    7. Метод удаления заявки Tracker.delete [#211749]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -67,4 +67,16 @@ public class Tracker {
         }
         return  rsl;
     }
+
+    public boolean delete(int id) {
+        int index = indexOf(id);
+        boolean rsl = false;
+        if (index > -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+            rsl = true;
+        }
+        return rsl;
+    }
 }
