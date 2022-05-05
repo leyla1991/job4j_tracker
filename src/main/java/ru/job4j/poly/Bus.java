e36e0cb179e797b6ee47a0c06b82e64f3d81cac5
leyla1991 <atagleyla@mail.ru>
2022-05-05 11:09:15 +0300

    3. Интерфейс Input [#181092 #297092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,14 +4,16 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
+        int numRoute = 451;
     }
 
     @Override
     public void passenger(int count) {
+        count = 40;
     }
 
     @Override
     public int refuel(int gaz, int price) {
-        return 0;
+        return gaz *= price;
     }
 }
