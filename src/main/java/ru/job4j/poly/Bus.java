dfbf3361527751b6e91e403110b0f6f8c9ab50c6
leyla1991 <atagleyla@mail.ru>
2022-05-05 14:21:52 +0300

    3. Интерфейс Input [#181092 #297092]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,12 +4,12 @@ public class Bus implements Transport {
 
     @Override
     public void drive() {
-        int numRoute = 451;
+        System.out.println("Маршрут автобуса 451");
     }
 
     @Override
     public void passenger(int count) {
-        count = 40;
+        System.out.println("Количество пассажиров в автобусе 40");
     }
 
     @Override
