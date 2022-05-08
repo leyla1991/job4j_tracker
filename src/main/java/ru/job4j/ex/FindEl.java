2bb48886337f4dfe3ba0584b80f42213f054c522
leyla1991 <atagleyla@mail.ru>
2022-05-08 19:32:29 +0300

    0.4. Пользовательские исключения. [#219367]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -4,13 +4,15 @@ public class FindEl {
 
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int rsl = -1;
-        if (key == null) {
-            throw new ElementNotFoundException("Key could not be null");
-        }
+
         for (int i = 0; i < value.length; i++) {
-            if (value[i] == key) {
+            if (value[i].equals(key)) {
                 rsl = i;
+                break;
             }
+        }
+        if (key == null && rsl == -1) {
+            throw new ElementNotFoundException("Key could not be null");
         }
         return rsl;
     }
