74b0cb25e97470a8db51091052a776d7d44576e4
leyla1991 <atagleyla@mail.ru>
2022-05-08 11:46:22 +0300

    9.2. Тесты вывода на консоль [#33585 #298493]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -161,7 +161,7 @@ public class StartUITest {
         Item one = tracker.add(new Item("test1"));
         Item two = tracker.add(new Item("test2"));
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(one.getId()), String.valueOf(two.getId()), "1"}
+                new String[] {"0", "1"}
         );
         UserAction[] actions = new UserAction[] {
                 new ShowAllAction(output),
