deb660e6bec686db672629983df9e67e0361471c
leyla1991 <atagleyla@mail.ru>
2022-05-12 13:41:50 +0300

    1. Singleton [#94606]

---
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -0,0 +1,46 @@
+package ru.job4j.tracker;
+
+public class SingleTracker {
+    private static SingleTracker single = null;
+
+    private SingleTracker() {
+
+    }
+
+    public static SingleTracker getSingle() {
+        if (single == null) {
+            single = new SingleTracker();
+        }
+        return single;
+    }
+
+    private Tracker tracker = new Tracker();
+
+    public Item add(Item item) {
+        return tracker.add(item);
+    }
+
+    public Item findById(int id) {
+        return null;
+    }
+
+    public Item[] findAll() {
+        return null;
+    }
+
+    public Item[] findByName(String key) {
+        return null;
+    }
+
+    private int indexOf(int id) {
+        return 0;
+    }
+
+    public boolean replace(int id, Item item) {
+        return false;
+    }
+
+    public boolean delete(int id) {
+        return false;
+    }
+}
