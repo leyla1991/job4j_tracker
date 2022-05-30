6b43fb55c71abd32bda232b08d10dd8c691593ce
leyla1991 <atagleyla@mail.ru>
2022-05-30 14:21:06 +0300

    0. Set, HashSet, Iterator. [#10095]

---
+++ src/main/java/ru/job4j/collection/UsageHashSet.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashSet;
+
+public class UsageHashSet {
+
+    public static void main(String[] args) {
+        HashSet<String> autos = new HashSet<>();
+        autos.add("LADA");
+        autos.add("BMV");
+        autos.add("VOLVO");
+        autos.add("TOYOTA");
+        for (String auto : autos) {
+            System.out.println(auto);
+        }
+    }
+}
