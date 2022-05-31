d68d46d98d57359493f61b08a23843235c435d60
leyla1991 <atagleyla@mail.ru>
2022-05-31 13:32:22 +0300

    1. Паспорт и жители [#212656]

---
+++ src/test/java/ru/job4j/collection/PassportOfficeTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+
+    @Test
+    public void whenDublicate() {
+        Citizen citizen = new Citizen("2332f", "Petr Arsentev");
+        Citizen dublicate = new Citizen("2332f", "Anatolyi Ivanov");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        office.add(dublicate);
+        assertFalse(false);
+    }
+}
