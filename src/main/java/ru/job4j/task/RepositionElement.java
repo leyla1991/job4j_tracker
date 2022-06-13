66f8ca2a60af1d43a8059dbad08fe3c7860fb3ce
leyla1991 <atagleyla@mail.ru>
2022-06-13 19:37:58 +0300

    0.6. Удаление и замена элементов в списке

---
+++ src/main/java/ru/job4j/task/RepositionElement.java
@@ -0,0 +1,13 @@
+package ru.job4j.task;
+
+import java.util.List;
+
+public class RepositionElement {
+    public static List<String> changePosition(List<String> list, int index) {
+    String s = list.remove(list.size() - 1);
+    if ( index < list.size()) {
+        list.set(index, s);
+    }
+    return list;
+}
+}
