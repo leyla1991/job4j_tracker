ba8553c693ec418427120ff3883d374e5125c5ce
leyla1991 <atagleyla@mail.ru>
2024-10-07 17:07:25 +0300

    2. @Setter, @Getter и другие [#504889]

--- src/main/java/ru/job4j/tracker/lombok/Category.java
+++ src/main/java/ru/job4j/tracker/lombok/Category.java
@@ -1,14 +1,11 @@
 package ru.job4j.tracker.lombok;
 
-
 import lombok.*;
-
 
 @Getter
 @RequiredArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Category {
-
 
     @EqualsAndHashCode.Include
     @NonNull
